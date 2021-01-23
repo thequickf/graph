4a6c5147da5a5bdbb1721dc6683221f7958b0f7c
Ivan Lopatin <ivan.lopatin@bricsys.com>
2021-01-24 03:09:14 +0700

    graph-struct-1: graph structure concept

--- src/edge.hpp
+++ src/edge.hpp
@@ -1,19 +1,23 @@
 #ifndef EDGE_HPP
 #define EDGE_HPP
 
+#include <utility>
+
 namespace graph {
 
 template<typename Node, typename... Types>
 class Edge : public Types... {
 public:
-    Edge(const Node& from, const Node& to, const Types&... args) : from(from), to(to), Types(args)... {}
+    Edge(Node from, Node to, Types... args) :
+        from(std::move(from)), to(std::move(to)), Types(std::move(args))... {}
 
     const Node from;
     const Node to;
 };
 
 template<typename Node, typename... Types>
-bool operator<(const Edge<Node, Types...>& lhe, const Edge<Node, Types...>& rhe) {
+bool operator<(const Edge<Node, Types...>& lhe, 
+               const Edge<Node, Types...>& rhe) {
     if (lhe.from != rhe.from)
         return lhe.from < rhe.from;
     return lhe.to < rhe.to;
--- src/graph.hpp
+++ src/graph.hpp
@@ -2,14 +2,45 @@
 #define GRAPH_HPP
 
 #include <edge.hpp>
+#include <impl/graph.hpp>
+#include <impl/meta_utils.hpp>
 
 namespace graph {
 
-template<typename Node, typename... Types>
-class Graph {
-public:
-    virtual void addEdge(const Edge<Node, Types...>& edge) = 0;
+class GraphTrait {
+protected:
+    GraphTrait() = default;
 };
+
+class EdgeTrait {
+protected:
+    EdgeTrait() = default;
+};
+
+// TODO: source and sink Node* type.
+// Should we inherit all nodes from base Node class
+// or should we apply more template woodoo?
+class Net : public GraphTrait {
+public:
+    Net(int source, int sink) : source(source), sink(sink) {}
+    int source, sink;
+};
+
+class Weighted : public EdgeTrait {
+public:
+    Weighted(uint32_t weight) : weight(weight) {}
+    uint32_t weight;
+};
+
+// TODO: Here I want to apply
+// requires ((std::is_base_of_v<graph::GraphTrait, Ts> || std::is_base_of_v<graph::EdgeTrait, Ts>) && ...)
+// but I wasn't able to make it work for now
+template<typename Node, typename... Traits>
+using Graph = graph_impl::Graph<
+    Node,
+    graph_impl::filter<graph_impl::graph_trait_condition, Traits...>,
+    graph_impl::filter<graph_impl::edge_trait_condition, Traits...>
+>;
 
 }  // graph
 
