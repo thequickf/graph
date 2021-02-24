bd92c07ec1596117de19b60725a430f024baef00
Ivan Lopatin <ivan.lopatin@bricsys.com>
2021-02-24 18:04:23 +0700

    graph-algo-0: added typed DsuConstructability tests

--- tests/utils_tests.cpp
+++ tests/utils_tests.cpp
@@ -1,14 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <graph_traits.hpp>
 #include <impl/base_utils.hpp>
 
+#include <functional>
 #include <string>
 
 namespace {
-/*
+
 struct NoDefaultConstructorComparable {
   NoDefaultConstructorComparable() = delete;
-  NoDefaultConstructorComparable(const std::string str) : str(str) {}
+  NoDefaultConstructorComparable(std::string str) : str(std::move(str)) {}
   std::string str;
 };
 
@@ -16,10 +18,65 @@ bool operator<(const NoDefaultConstructorComparable& left,
                const NoDefaultConstructorComparable& right) {
   return left.str < right.str;
 }
-//*/
+
+struct NoDefaultConstructorHashable {
+  NoDefaultConstructorHashable() = delete;
+  NoDefaultConstructorHashable(std::string str) : str(std::move(str)) {}
+  std::string str;
+};
+
+bool operator==(const NoDefaultConstructorHashable& left,
+                const NoDefaultConstructorHashable& right) {
+  return left.str == right.str;
+}
+
 }  // namespace
 
+namespace std {
+
+template<>
+struct hash<NoDefaultConstructorHashable> {
+  std::size_t operator()(
+      const NoDefaultConstructorHashable& str) const noexcept {
+    return std::hash<std::string>{}(str.str);
+  }
+};
+
+}  // std
+
 namespace graph_tests {
+
+template <typename T>
+class DsuStruct : public ::testing::Test {};
+
+TYPED_TEST_SUITE_P(DsuStruct);
+
+TYPED_TEST_P(DsuStruct, DsuConstructability) {
+  const std::string s1 = "first";
+  const std::string s2 = "second";
+  const std::string s3 = "third";
+  const std::string s4 = "fourth";
+  const std::string su = "unknown";
+  TypeParam dsu({s1, s2, s3});
+  EXPECT_FALSE(dsu.Add(s1));
+  EXPECT_TRUE(dsu.Add(s4));
+  EXPECT_TRUE(dsu.Unite(s1, s2));
+  EXPECT_FALSE(dsu.Unite(s1, s2));
+  EXPECT_FALSE(dsu.Size(su));
+  EXPECT_FALSE(dsu.Unite(s1, su));
+  EXPECT_EQ(*dsu.Size(s1), 2);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(DsuStruct, DsuConstructability);
+
+using DsuSetTypes = testing::Types<
+    graph_utils::Dsu<std::string>,
+    graph_utils::Dsu<std::string, graph::HashTableBased>,
+    graph_utils::Dsu<NoDefaultConstructorComparable>,
+    graph_utils::Dsu<NoDefaultConstructorHashable, graph::HashTableBased>
+>;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(DsuConstructability, DsuStruct, DsuSetTypes);
 
 TEST(DsuUtil, IntDsuValidity) {
   graph_utils::Dsu<int> dsu({1, 2, 3, 4, 5});
